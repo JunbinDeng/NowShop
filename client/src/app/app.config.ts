642aa79bce63e2e28328d595ddf620354a949418
Junbin Deng <junbin.deng@gmail.com>
2025-03-31 09:27:49 +1300

    Add cart features and environment configurations

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,15 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptor/error.interceptor';
 import { loadingInterceptor } from './core/interceptor/loading.interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
+
+function initializeApp(InitService: InitService) {
+  return () =>
+    lastValueFrom(InitService.init()).finally(() => {
+      const splash = document.getElementById('initial-splash');
+      if (splash) {
+        splash.remove();
+      }
+    });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(() => {
+      return initializeApp(new InitService())();
+    }),
   ],
 };
